3968ff153361a91c86b29f6baefe89dea110c72c
Kai Hendry <hendry@webconverger.com>
2014-11-17 18:50:31 +0800

    Save us sometime if vipsthumbnail is available

--- thumb.go
+++ thumb.go
@@ -1,15 +1,33 @@
 package main
 
 import (
+	"fmt"
 	"image/jpeg"
 	"log"
 	"os"
+	"os/exec"
 	"path/filepath"
 
 	"github.com/nfnt/resize"
 )
 
 func genthumb(src string, dst string) (err error) {
+
+	dir, _ := filepath.Split(dst)
+	err = os.MkdirAll(dir, 0700)
+	if err != nil {
+		return
+	}
+
+	path, err := exec.LookPath("vipsthumbnail")
+	if err == nil {
+		out, err := exec.Command(path, "-s", "460x460", "-o", dst, src).CombinedOutput()
+		if err != nil {
+			fmt.Printf("The output is %s\n", out)
+			log.Fatal(err)
+		}
+		return err
+	}
 
 	file, err := os.Open(src)
 	if err != nil {
@@ -24,11 +42,6 @@ func genthumb(src string, dst string) (err error) {
 
 	m := resize.Thumbnail(460, 460, img, resize.NearestNeighbor)
 
-	dir, _ := filepath.Split(dst)
-	err = os.MkdirAll(dir, 0700)
-	if err != nil {
-		return
-	}
 	out, err := os.Create(dst)
 	if err != nil {
 		log.Fatal(err)
