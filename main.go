0e9a55bffbb9ca81a0ad68bfa1a00b31a0a37272
Konstantin Levinski <konstantin.levinski@gmail.com>
2016-06-07 22:29:07 +0800

    Path checking

--- main.go
+++ main.go
@@ -52,6 +52,9 @@ func main() {
 
 	directory := flag.Arg(0)
 	dirPath, _ = filepath.Abs(directory)
+	
+	// Getting rid of /../ etc
+	dirPath = path.Clean(dirPath)
 
 	// Don't allow path under dirPath to be viewed
 	http.Handle("/o/", http.StripPrefix(path.Join("/o", dirPath), http.FileServer(http.Dir(dirPath))))
@@ -79,16 +82,19 @@ func main() {
 
 func thumb(w http.ResponseWriter, r *http.Request) {
 
+	// Path cleaning
+	requestedPath := path.Clean(r.URL.Path[2:])
+
 	// Make sure you can't go under the dirPath
-	if !strings.HasPrefix(r.URL.Path[2:], dirPath) {
+	if !strings.HasPrefix(requestedPath, dirPath) {
 		http.NotFound(w, r)
 		return
 	}
 
-	thumbPath := filepath.Join(dirThumbs, r.URL.Path[2:])
+	thumbPath := filepath.Join(dirThumbs, requestedPath)
 	if _, err := os.Stat(thumbPath); err != nil {
 		log.Println("THUMB:", thumbPath, "does not exist")
-		srcPath := r.URL.Path[2:]
+		srcPath := requestedPath
 		if _, err := os.Stat(srcPath); err != nil {
 			log.Println("ORIGINAL", srcPath, "does not exist")
 			http.NotFound(w, r)
