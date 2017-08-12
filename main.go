5de96f9fe012e9894deef7b9924f96dd8d9c806c
Kai Hendry <hendry@iki.fi>
2017-08-13 06:52:36 +0800

    Show PNG and MP4s, many thanks to #go-nuts

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"flag"
 	"fmt"
 	"html/template"
-	"io/ioutil"
 	"log"
 	"net"
 	"net/http"
@@ -16,6 +15,11 @@ import (
 	"github.com/skratchdot/open-golang/open"
 )
 
+type media struct {
+	filename string
+	f        os.FileInfo
+}
+
 func in(slice []string, str string) bool {
 	for _, s := range slice {
 		if s == str {
@@ -25,7 +29,7 @@ func in(slice []string, str string) bool {
 	return false
 }
 
-var acceptedImageExt = []string{".jpg", ".jpeg"}
+var acceptedImageExt = []string{".jpg", ".jpeg", ".mp4", ".png"}
 var dirThumbs = fmt.Sprintf("%s%s", os.Getenv("HOME"), "/.cache/lk")
 var dirPath = "."
 var gitVersion string
@@ -113,42 +117,33 @@ func thumb(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, thumbPath)
 }
 
+func findmedia(m *[]media) func(filename string, f os.FileInfo, err error) error {
+	return func(filename string, f os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		// log.Printf("Visited: %s\n", filename)
+		if !f.IsDir() && in(acceptedImageExt, strings.ToLower(path.Ext(filename))) {
+			// log.Printf("Adding: %s\n%+v\n", filename, f)
+			*m = append(*m, media{filename, f})
+		}
+		return nil
+	}
+}
+
 func lk(w http.ResponseWriter, r *http.Request) {
 
-	// log.Println("dirPath", dirPath, "Web path", r.URL.Path)
 	srcPath := filepath.Join(dirPath, r.URL.Path)
-	// log.Println("Combined", srcPath)
 
-	files, err := ioutil.ReadDir(srcPath)
-	// log.Println(files)
-
+	var m []media
+	err := filepath.Walk(srcPath, findmedia(&m))
 	if err != nil {
-		panic(err)
+		log.Println(err)
 	}
 
-	dirs := []string{}
-	images := []string{}
+	t := template.New("medialist")
 
-	for _, f := range files {
-
-		if strings.HasPrefix(filepath.Base(f.Name()), ".") {
-			continue
-		}
-
-		// log.Println("Filename", f.Name())
-		if f.IsDir() {
-			// log.Println(f.Name(), "is a directory")
-			// TODO check they have a JPG in them?
-			dirs = append(dirs, filepath.Join(r.URL.Path, f.Name()))
-		}
-		// Only append jpg images
-		if in(acceptedImageExt, strings.ToLower(path.Ext(f.Name()))) {
-			log.Printf("Appending %s", f.Name())
-			images = append(images, filepath.Join(srcPath, f.Name()))
-		}
-	}
-
-	t, err := template.New("foo").Parse(`<!DOCTYPE html>
+	template.Must(t.Funcs(template.FuncMap{"markMedia": markMedia}).Parse(`<!DOCTYPE html>
 <html>
 <head>
 <meta charset="utf-8" />
@@ -160,27 +155,17 @@ li a { flex: 1; border: thin dotted black; text-decoration: none; padding: 0.3em
 </style>
 </head>
 <body>
-<ol>
-{{ range .Dirs }}<li><a href="{{ . }}">{{ . }}</a></li>
-{{ end }}
-</ol>
-{{ range .Images }}<a title="{{ . }}" href="/o{{ . }}"><img alt="" width=230 height=230 src="/t{{ . }}"></a>
+{{ range .Media }}{{ . | markMedia }}
 {{ end }}
 <p>By <a href=https://github.com/kaihendry/lk>lk {{ .Version }}</a></p>
 </body>
-</html>`)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
+</html>`))
 
 	data := struct {
-		Images  []string
-		Dirs    []string
+		Media   []media
 		Version string
 	}{
-		images,
-		dirs,
+		m,
 		gitVersion,
 	}
 
@@ -188,4 +173,20 @@ li a { flex: 1; border: thin dotted black; text-decoration: none; padding: 0.3em
 
 	log.Printf("%s %s %s %s\n", r.RemoteAddr, r.Method, r.URL, r.UserAgent())
 
+}
+
+func markMedia(m media) template.HTML {
+	switch strings.ToLower(path.Ext(m.filename)) {
+	case ".jpg":
+		s := fmt.Sprintf("<a title=\"%s\" href=\"/o%s\"><img alt=\"\" width=230 height=230 src=\"/t%s\"></a>", m.filename, m.filename, m.filename)
+		return template.HTML(s)
+	case ".png":
+		s := fmt.Sprintf("<a title=\"%s\" href=\"/o%s\"><img alt=\"\" width=230 height=230 src=\"/o%s\"></a>", m.filename, m.filename, m.filename)
+		return template.HTML(s)
+	case ".mp4":
+		s := fmt.Sprintf("<video controls width=230 height=230 src=\"/o%s\"></video>", m.filename)
+		return template.HTML(s)
+	default:
+		return template.HTML(m.f.Name())
+	}
 }
